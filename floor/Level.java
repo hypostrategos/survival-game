aa6af66ba24de2ab027ee94b54be0ff0b26eea62
bismack <hypostrategosdev@gmail.com>
2017-10-01 05:32:23 -0400

    added room

--- floor/Level.java
+++ floor/Level.java
@@ -3,8 +3,8 @@ import java.util.*;
 import java.util.stream.*;
 
 public class Level {
-	public static final int totalTiles = 400;
-	public static final int tileWidth = 20;
+	public static final int totalTiles = 100;
+	public static final int tileWidth = 10;
 	public static int lowestFloor = 0;
 	public static Random rand = new Random();
 	
@@ -21,33 +21,19 @@ public class Level {
 		.limit(totalTiles)
 		.forEach(x->tileList.add(new Tile(x)));
 
-		path().stream()
+		Set<Integer> path = new TreeSet<>();
+		setPath(path).stream()
 		.forEach(trav->tileList.get(trav).setTraversable(true));
 
 		tileList.stream()
 		.forEach(System.out::print);
 	}
-	public Set<Integer> path() {
-		Set<Integer> path = new TreeSet<>();
+	public Set<Integer> setPath(Set<Integer> path) {
 		int maxWidth=tileWidth-1, maxHeight=totalTiles/tileWidth-1;
-		int x=tileWidth/2, y=totalTiles/(2*tileWidth);
+		int x=rand.nextInt(tileWidth), y=rand.nextInt(maxHeight+1);
 		int counter=0;
 
-		// Stream.iterate(0, iter->iter+1)
-		// .limit(totalTiles-1)
-		// .filter(tile->rand.nextInt(10)>8)
-		// .forEach(tile-> {
-		// 	path.add(Math.max(tile-1-maxWidth,0));
-		// 	path.add(Math.max(tile-maxWidth,0));
-		// 	path.add(Math.max(tile+1-maxWidth,0));
-		// 	path.add(Math.max(tile-1,0));
-		// 	path.add(tile);
-		// 	path.add(Math.min(tile+1,totalTiles));
-		// 	path.add(Math.min(tile-1+maxWidth,totalTiles-1));
-		// 	path.add(Math.min(tile+maxWidth,totalTiles-1));
-		// 	path.add(Math.min(tile+1+maxWidth,totalTiles-1));
-		// } );
-		while(counter<totalTiles/4) {
+		while(counter<totalTiles) {
 			counter++;
 			// System.out.print(x+" "+y+" "+(x+y*tileWidth)+" -> ");
 			x=Math.min(Math.max((x-1+rand.nextInt(3)),0),maxWidth);
---
+++ floor/Room.java
@@ -0,0 +1,5 @@
+public class Room {
+	public Room() {
+
+	}
+}
