d1edc597fec053e266eb727a432127c85ebb60fc
bismack <hypostrategosdev@gmail.com>
2017-10-05 23:29:36 -0400

    switch to array

--- floor/Level.java
+++ floor/Level.java
@@ -6,9 +6,11 @@ public class Level {
 	public static final int totalTiles = 100;
 	public static final int tileWidth = 10;
 	public static int lowestFloor = 0;
+	public static int numberOfRooms = 2;
 	public static Random rand = new Random();
 	
-	private List<Tile> tileList;
+	private int[] tileList;
+	private List<Room> roomList;
 	private int floor;
 
 	public Level() {
@@ -16,34 +18,41 @@ public class Level {
 		levelGen();
 	}
 	public void levelGen() {
-		tileList = new ArrayList<>(totalTiles);
-		Stream.iterate(0, x->x+1)
-		.limit(totalTiles)
-		.forEach(x->tileList.add(new Tile(x)));
-
-		Set<Integer> path = new TreeSet<>();
-		setPath(path).stream()
-		.forEach(trav->tileList.get(trav).setTraversable(true));
-
-		tileList.stream()
-		.forEach(System.out::print);
-	}
-	public Set<Integer> setPath(Set<Integer> path) {
-		int maxWidth=tileWidth-1, maxHeight=totalTiles/tileWidth-1;
-		int x=rand.nextInt(tileWidth), y=rand.nextInt(maxHeight+1);
-		int counter=0;
-
-		while(counter<totalTiles) {
-			counter++;
-			// System.out.print(x+" "+y+" "+(x+y*tileWidth)+" -> ");
-			x=Math.min(Math.max((x-1+rand.nextInt(3)),0),maxWidth);
-			y=Math.min(Math.max((y-1+rand.nextInt(3)),0),maxHeight);
-			path.add(x+y*tileWidth);
-			// System.out.println(" "+x+" "+y+" "+(x+y*tileWidth));
+		tileList = new int[totalTiles];
+		roomList = new ArrayList<>(numberOfRooms);
+		for (int i=0;i<numberOfRooms;i++) {
+			roomList.add(new Room(tileList));
 		}
+		Arrays.stream(tileList)
+		.forEach(System.out::println);
 
-		return path;
+		// Stream.iterate(0, x->x+1)
+		// .limit(totalTiles)
+		// .forEach(x->tileList.add(new Tile(x)));
+
+		// Set<Integer> path = new TreeSet<>();
+		// setPath(path).stream()
+		// .forEach(trav->tileList.get(trav).setTraversable(true));
+
+		// tileList.stream()
+		// .forEach(System.out::print);
 	}
+	// public Set<Integer> setPath(Set<Integer> path) {
+	// 	int maxWidth=tileWidth-1, maxHeight=totalTiles/tileWidth-1;
+	// 	int x=rand.nextInt(tileWidth), y=rand.nextInt(maxHeight+1);
+	// 	int counter=0;
+
+	// 	while(counter<totalTiles) {
+	// 		counter++;
+	// 		// System.out.print(x+" "+y+" "+(x+y*tileWidth)+" -> ");
+	// 		x=Math.min(Math.max((x-1+rand.nextInt(3)),0),maxWidth);
+	// 		y=Math.min(Math.max((y-1+rand.nextInt(3)),0),maxHeight);
+	// 		path.add(x+y*tileWidth);
+	// 		// System.out.println(" "+x+" "+y+" "+(x+y*tileWidth));
+	// 	}
+
+	// 	return path;
+	// }
 	public void layout(int size) {
 	}
 }
--- floor/Room.java
+++ floor/Room.java
@@ -1,5 +1,22 @@
-public class Room {
-	public Room() {
+package floor;
 
+public class Room {
+	private final int roomSize=4;
+
+	public Room(int[] tileList) {
+		int origin;
+		while (tileList[origin=Level.rand.nextInt(tileList.length)]!=1) {
+			tileList[origin]=1;
+			break;
+		}
+		int x=0, y=0;
+		for (int j=0; j<roomSize; j++) {
+			y=Math.min((origin+j*Level.tileWidth),Level.totalTiles/Level.tileWidth);
+			for (int i=0; i<roomSize; i++) {
+				x=Math.min((origin+i),Level.tileWidth-1);
+				tileList[x+y-origin]=1;
+				System.out.println(x+" "+y);
+			}
+		}
 	}
 }
